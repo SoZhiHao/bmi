7b5f5eac92683032c185c79500d79cd4f662cf4b
SoZhiHao <sozh-wp22@student.tarc.edu.my>
2025-02-26 15:37:13 +0800

    Main.dart modified.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BMI',
+      title: 'title 2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
